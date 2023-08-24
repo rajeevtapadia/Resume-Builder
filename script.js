fea766fcedbd54936b2375ad79598d3e9e23fa71
Rajeev Tapadia <rtapadia730@gmail.com>
2023-08-24 12:26:23 +0530

    fetched all resume fields

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// get all elements
+// get all resume fields
+const resumeFields = document.getElementsByClassName('resume-field')
+console.log(resumeFields)
+
+// form button functionality
 
 
 // get all form data
+// const formData = document.querySelectorAll()
 
 
 // save all data to localstorage
